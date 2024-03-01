aaf4da4fffd2898689462b17ee919ea02c288bc8
Prayas Chavda <141898696+Caffeinechic@users.noreply.github.com>
2024-03-01 12:04:15 +0530

    Add files via upload
    Uploaded Files

---
+++ Assignment 2/ArtHub/script.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    alert("Welcome to Art Hub!");
+};
+
+window.onload = function() {
+    var welcomeMessage = document.getElementById('welcome-message');
+    if (welcomeMessage) {
+        welcomeMessage.innerHTML = 'Explore and Share Your Artistic Journey!';
+    }
+};
